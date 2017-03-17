5615d0f875c920e9f9704781eedd91d4ea06aa70
danionita <Dan@UTWKS21432.ad.utwente.nl>
2017-03-17 23:44:17 +0100

    MS actors added to profitability tables

--- src/main/java/e3fraud/gui/TableGenerator.java
+++ src/main/java/e3fraud/gui/TableGenerator.java
@@ -9,6 +9,7 @@ import com.hp.hpl.jena.rdf.model.Resource;
 import e3fraud.model.E3Model;
 import e3fraud.vocabulary.E3value;
 import java.util.List;
+import java.util.Set;
 import javax.swing.JTable;
 
 /**
@@ -16,12 +17,27 @@ import javax.swing.JTable;
  * @author Dan
  */
 public class TableGenerator {
+    /**
+     * Creates a "profitability" table showing the financial result of all actors.
+     * Each table contains 4 columns:
+     * <ul>
+     *  <li> Actor: the name of the actor </li>
+     *  <li> Result: the financial result of the actor in the fraudModel</li>
+     *  <li> Expected result: the financial result of the actor in the valueModel</li>
+     *  <li> Loss/Gain: the difference between Result and Expected result</li>
+     * </ul>
+     * @param fraudModel the fraudModel to be used for computing the Result column
+     * @param valueModel the valueModel to be used for computing the Expected result column
+     * @return a 4-column JTable with headers and one row per actor
+     */
+    
     public static JTable generateTable(E3Model fraudModel, E3Model valueModel){
-    Object columnNames[] = { "Actor", "Result", "Expected result", "Loss/Gain"};       
-    Object rowData[][] = new Object[fraudModel.getActors().size()][4];
+    Object columnNames[] = { "Actor", "Result", "Expected result", "Loss/Gain"};
+    Set<Resource> actorsAndMarketSegments = fraudModel.getActorsAndMarketSegments();   
+    Object rowData[][] = new Object[actorsAndMarketSegments.size()][4];
     
     int row=0;
-    for (Resource actor : fraudModel.getActors()){
+    for (Resource actor : actorsAndMarketSegments){
         rowData[row][0] = actor.getProperty(E3value.e3_has_name).getLiteral().toString();
         
         double result = fraudModel.getTotalForActor(actor, false);
@@ -47,18 +63,29 @@ public class TableGenerator {
         row++;
     }    
         
-    
-    
+
     JTable table = new JTable(rowData, columnNames);
     return table;
     }
     
+    
+    /**
+     * Creates a "profitability" table showing the financial result of all actors.
+     * Each table contains 4 columns:
+     * <ul>
+     *  <li> Actor: the name of the actor </li>
+     *  <li> Result: the financial result of the actor in the valueModel</li>
+     * </ul>
+     * @param valueModel the valueModel to be used for computing the Expected result column
+     * @return a 2-column JTable with headers and one row per actor
+     */
    public static JTable generateTable(E3Model valueModel){
-    Object columnNames[] = { "Actor", "Result" };       
-    Object rowData[][] = new Object[valueModel.getActors().size()][2];
+    Object columnNames[] = { "Actor", "Result" };           
+    Set<Resource> actorsAndMarketSegments = valueModel.getActorsAndMarketSegments();   
+    Object rowData[][] = new Object[actorsAndMarketSegments.size()][2];
     
     int row=0;
-    for (Resource actor : valueModel.getActors()){
+    for (Resource actor : actorsAndMarketSegments){
         rowData[row][0] = actor.getProperty(E3value.e3_has_name).getLiteral().toString();
         
         double result = valueModel.getTotalForActor(actor, false);
